981ef1881da01eaca5d986b723f3d029c583e1ba
peterkchung <77805646+peterkchung@users.noreply.github.com>
2024-01-12 13:14:52 -0500

    ADD agent class template, conversation struture to models

---
+++ src/agents/base.py
@@ -0,0 +1,30 @@
+import os
+from huggingface_hub import InferenceClient
+
+"""
+research agent
+text-classification > action
+
+https://huggingface.co/docs/huggingface_hub/v0.20.2/en/package_reference/inference_client#huggingface_hub.InferenceClient.text_classification
+
+:params:
+:text (str) — A string to be classified.
+
+:model (str, optional) — The model to use for the text classification task. Can be a model ID hosted on the Hugging Face Hub or a URL to a deployed Inference Endpoint. If not provided, the default recommended text classification model will be used. Defaults to None.
+
+"""
+class Tool():
+    def __init__(self):
+        self
+    
+class Agent():
+    def __init__(self):
+        self
+    
+    def _evaluate_query():
+        # Evaluate query to assign task
+        return None
+    
+    def get_task():
+        # some functcion    
+        return None
